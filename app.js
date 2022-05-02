572ac5574885e511e065b9523250235810d01902
Alesvoy <alesvoy@gmail.com>
2022-05-01 21:19:34 -0500

    Add javascript to hide and show accordion content

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ for (let i = 0; i < listEl.children.length; i++) {
   const boxEl = listEl.children[i].children[0];
   const textEl = listEl.children[i].children[1];
 
+  let open = false;
+
   boxEl.addEventListener('click', () => {
     textEl.classList.toggle('hide');
+
+    if (open) {
+      boxEl.children[1].classList.remove('rotateUp');
+      boxEl.children[1].classList.add('rotateDown');
+      open = false;
+      console.log(boxEl.children[1]);
+      return;
+    }
+
+    boxEl.children[1].classList.add('rotateUp');
+    boxEl.children[1].classList.remove('rotateDown');
+    open = true;
+
+    console.log(boxEl.children[1]);
   });
 
   console.log(listEl.children[i].children[1]);
